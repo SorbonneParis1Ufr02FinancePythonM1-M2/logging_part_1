f807df7ce65c52fccdd6a0a0e7fcf236d8c8e878
fabgal-p1 <>
2025-03-10 10:12:56 +0100

    - console logger and file logger with separate levels

--- helpers_logging.py
+++ helpers_logging.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 
 # logging_level = CRITICAL, ERROR, WARNING, INFO, DEBUG
-logging_level = "DEBUG"
+console_logging_level = "DEBUG"
+file_logging_level = "DEBUG"
 log_file_path = "app.log"
 
 
 def log_message(message, level):
     levels = {"CRITICAL": 50, "ERROR": 40, "WARNING": 30, "INFO": 20, "DEBUG": 10}
 
-    if levels.get(level, "DEBUG") >= levels[logging_level]:
+    _level = levels.get(level, "DEBUG")
+    if _level >= levels[console_logging_level]:
         print(f"{level} | {message}")
 
+    if _level >= levels[file_logging_level]:
         with open(log_file_path, "a") as log_file:
             log_file.write(f"{datetime.now()} | {level} | {message}\n")
 
